6d137396c75988d8f218bd04a3597fa5de03b0fc
Parvati Jayakumar <51737416+parvatijay2901@users.noreply.github.com>
2022-07-17 22:54:02 +0530

    Update convert_text_to_phn_exp.py

--- TTS/Parallel-WaveGAN/convert_text_to_phn_exp.py
+++ TTS/Parallel-WaveGAN/convert_text_to_phn_exp.py
@@ -2,13 +2,9 @@
 
 from espnet2.text.phoneme_tokenizer import PhonemeTokenizer
 
-
 def get_lex(line):
-
     g2p="espeak_ng_hindi"
-    tokenizer = PhonemeTokenizer(
-        g2p_type=g2p
-    )
+    tokenizer = PhonemeTokenizer(g2p_type=g2p)
     tokens = tokenizer.text2tokens(line)
     print(" ".join(tokens) + "\n")
     return(" ".join(tokens) + "\n")
